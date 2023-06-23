25aab565a81f237eaace2299bdbe964cc09c1af2
Artur Kuch <kucharthur18@gmail.com>
2023-06-23 16:34:47 +0300

    -Added tests for FoodService

---
+++ src/test/java/com/kuch/Fooddelivery/api/service/FoodServiceTests.java
@@ -0,0 +1,81 @@
+package com.kuch.Fooddelivery.api.service;
+
+import com.kuch.Fooddelivery.api.util.FoodDataTestUtil;
+import com.kuch.Fooddelivery.dto.FoodDto;
+import com.kuch.Fooddelivery.entity.Food;
+import com.kuch.Fooddelivery.repository.FoodRepostiory;
+import com.kuch.Fooddelivery.service.impl.FoodServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Artur Kuch
+ */
+
+@ExtendWith(MockitoExtension.class)
+public class FoodServiceTests {
+
+    @Mock
+    FoodRepostiory foodRepostiory;
+
+    @InjectMocks
+    FoodServiceImpl foodService;
+
+    @Test
+    public void FoodService_CreateFood_ReturnsFoodDto(){
+        Food food = FoodDataTestUtil.createFood();
+        FoodDto foodDto = FoodDataTestUtil.createFoodDto();
+
+        when(foodRepostiory.save(Mockito.any(Food.class))).thenReturn(food);
+
+        FoodDto saved = foodService.createFood(foodDto);
+
+        assertThat(saved).isNotNull();
+    }
+
+    @Test
+    public void FoodService_GetFoodById_ReturnsFoodDto(){
+
+        Food food = FoodDataTestUtil.createFood();
+
+        when(foodRepostiory.findById(1)).thenReturn(Optional.ofNullable(food));
+
+        FoodDto existed = foodService.getFood(1);
+
+        assertThat(existed).isNotNull();
+    }
+
+    @Test
+    public void FoodService_updateFood_ReturnsFoodDto(){
+        Food food = FoodDataTestUtil.createFood();
+        FoodDto foodDto = FoodDataTestUtil.createFoodDto();
+
+        when(foodRepostiory.findById(1)).thenReturn(Optional.ofNullable(food));
+        when(foodRepostiory.save(Mockito.any(Food.class))).thenReturn(food);
+
+        FoodDto updated = foodService.updateFood(1, foodDto);
+
+        assertThat(updated).isNotNull();
+    }
+
+    @Test
+    public void FoodService_DeleteById_ReturnsVoid(){
+        Food food = FoodDataTestUtil.createFood();
+
+        when(foodRepostiory.findById(1)).thenReturn(Optional.ofNullable(food));
+
+        assertAll(() -> foodService.deleteFood(1));
+    }
+
+}
---
+++ src/test/java/com/kuch/Fooddelivery/api/util/FoodDataTestUtil.java
@@ -0,0 +1,32 @@
+package com.kuch.Fooddelivery.api.util;
+
+import com.kuch.Fooddelivery.dto.FoodDto;
+import com.kuch.Fooddelivery.entity.Food;
+import com.kuch.Fooddelivery.utils.mappers.FoodMapper;
+import fr.xebia.extras.selma.Selma;
+
+/**
+ * @author Artur Kuch
+ */
+public class FoodDataTestUtil {
+
+    private static final FoodMapper foodMapper = Selma.getMapper(FoodMapper.class);
+
+
+    public static Food createFood(){
+
+        return Food.builder()
+                .foodId(1)
+                .name("Food")
+                .description("Desc")
+                .price(1.0)
+                .quantity(1)
+                .build();
+
+    }
+
+    public static FoodDto createFoodDto(){
+        return foodMapper.asFoodDto(createFood());
+    }
+
+}
